5eba94e88f386aa4ade1835e09a10f5ed4c6ea7c
Elaine Fang <efang@weddingwire.com>
2017-01-27 17:41:48 -0500

    Fix bug

--- app/assets/javascripts/components/_item.js.jsx
+++ app/assets/javascripts/components/_item.js.jsx
@@ -6,9 +6,9 @@ var Item = React.createClass({
     handleEdit() {
         if(this.state.editable) {
             var description = this.refs.description.value;
-            var id = this.props.item.id;
             var due = this.refs.due.value;
             var person = this.refs.person.value;
+            var id = this.props.item.id;
             var item = {id: id , description: description , due: due , person: person};
             this.props.handleUpdate(item);
         }
@@ -17,17 +17,16 @@ var Item = React.createClass({
 
     render() {
         var description = this.state.editable ? <input type='type' ref='description' defaultValue={this.props.item.description} /> : <p><b>Item {this.props.item.id}:</b> {this.props.item.description}</p>;
-        var due = this.state.editable ? <input type='type' ref='description' defaultValue={this.props.item.due} /> : <span><b>Due by:</b> {this.props.item.due} &nbsp;</span>;
-        var person = this.state.editable ? <input type='type' ref='description' defaultValue={this.props.item.person} /> : <span><b>Assignee:</b> {this.props.item.person}</span>;
+        var due = this.state.editable ? <input type='type' ref='due' defaultValue={this.props.item.due} /> : <span><b>Due by:</b> {this.props.item.due} &nbsp;</span>;
+        var person = this.state.editable ? <input type='type' ref='person' defaultValue={this.props.item.person} /> : <span><b>Assignee:</b> {this.props.item.person} &nbsp;</span>;
         return (
             <div>
                 {description}
                 {due}
                 {person}
-                <p>
-                    <button onClick={this.props.handleDelete} >Delete</button>
-                    <button onClick={this.handleEdit}> {this.state.editable ? 'Submit' : 'Edit' } </button>
-                </p>
+                <button onClick={this.props.handleDelete} >Delete</button>
+                &nbsp;
+                <button onClick={this.handleEdit}> {this.state.editable ? 'Submit' : 'Edit' } </button>
             </div>
         )
     }
